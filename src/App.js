a02d7b5dbdc95d2851f4d7e58f4b8d86a66836e8
mayank0508 <thakurmayank0508@gmail.com>
2021-10-19 00:26:17 +0530

    bug solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myage: 20
+    };  
+  }
+  handleClick = () => {
+    this.setState((prevState, prevProps) => {
+      return { myage: prevState.myage + prevProps.increment }; // here we are using the prevProps and preState the .increment is coming from the index.js
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.myage}</p>
+          <button onClick={this.handleClick}>My age</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
